c68cffd074c475ccfb9ad975d9f512f3ead3246c
Petar Maymounkov <petarm@gmail.com>
2025-06-08 11:26:35 -0700

    fix

--- rpc/transport.go
+++ rpc/transport.go
@@ -113,8 +113,8 @@ func NewStreamTransport(c net.Conn) *StreamTransport {
 
 func (tx *StreamTransport) NewMessage(peer string) *Message {
 	buf := tx.mpool.Get().(*bytes.Buffer)
+	buf.Reset()
 	return NewMessage(peer, buf, func() {
-		buf.Reset()
 		tx.mpool.Put(buf)
 	})
 }
