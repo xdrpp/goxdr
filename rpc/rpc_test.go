5b659fb9c3a9bd3655570d3d3a5d72ce70d12827
Petar Maymounkov <petarm@gmail.com>
2025-06-08 15:27:49 -0700

    move to a global pool for rpc.Message buffers

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -60,7 +60,7 @@ func TestChannels(t *testing.T) {
 	contents := []string{"one\n", "two\n", "three\n"}
 	var ms []*rpc.Message
 	for _, msg := range contents {
-		m := rpc.NewMessage("", &bytes.Buffer{}, func() {})
+		m := rpc.NewMessage("")
 		m.WriteString(msg)
 		ms = append(ms, m)
 	}
